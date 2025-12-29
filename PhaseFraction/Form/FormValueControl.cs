fade0e2723efca1adceae98b00dba1324c96cc76
E E <492394013@qq.com>
2025-12-29 19:23:07 +0800

    电磁阀

---
+++ PhaseFraction/Form/FormValueControl.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PhaseFraction
+{
+    public partial class FormValueControl : Form
+    {
+        public FormValueControl()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnByPassValue_Click(object sender, EventArgs e)
+        {
+            if (BtnByPassValue.Text == "打开旁路电磁阀")
+            {
+                BtnByPassValue.Text = "关闭旁路电磁阀";
+                
+            }
+            else
+            {
+                BtnByPassValue.Text = "打开旁路电磁阀";
+                
+            }
+        }
+    }
+}
